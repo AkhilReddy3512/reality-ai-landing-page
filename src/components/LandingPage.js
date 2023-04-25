e6c94c2c6cdd4bd83ea1a321d19fbc921e8077f2
Akhileswar Reddy <119875281+AkhilReddy3512@users.noreply.github.com>
2023-04-25 15:09:23 +0530

    rebuild

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -20,7 +20,7 @@ function LandingPage() {
               <div className="card" style={{ width: "20%", marginTop: "2%", marginLeft: "2%" }}>
                 <img src="https://images.ctfassets.net/wdjnw2prxlw8/6HRSjw4NJnvoQEKuDF9BsM/25bd19e9dfbfa9be0137096f74c454fa/glass_buildings_view_from_street_to_sky.jpg" className="card-img-top" alt="project" />
                 <div className="card-body">
-                  <h6 className="card-title">Project</h6>
+                  <h6 className="card-title">Project1</h6>
                   <p className="card-text">RealtyAi</p>
                   <Link className="my-button" to="http://localhost:3001/" role="button" target='_blank'>Compare Now</Link>
                 </div>
