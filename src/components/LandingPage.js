cf802a878ff99fce578f38143b1fc802568b4631
Akhileswar Reddy <119875281+AkhilReddy3512@users.noreply.github.com>
2023-04-26 19:00:23 +0530

    added url

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,20 +9,18 @@ function LandingPage() {
       <h1 className="text1 text3"><b>RealtyAi</b></h1>
       </nav>
       <div className="App2">
-        <br /><br />
-        <p className="text">RealtyAi</p>
-        <br /><br />
+        <p style={{paddingTop:"9vw"}} className="text">RealtyAi</p>
       </div>
       <div>
-        <div>
+        <div class="container">
           <div className="row">
-            <div className="col">
-              <div className="card" style={{ width: "20%", marginTop: "2%", marginLeft: "2%" }}>
-                <img src="https://images.ctfassets.net/wdjnw2prxlw8/6HRSjw4NJnvoQEKuDF9BsM/25bd19e9dfbfa9be0137096f74c454fa/glass_buildings_view_from_street_to_sky.jpg" className="card-img-top" alt="project" />
+            <div className="col col-xs-6 col-md-4 col-xl-3">
+              <div className="card" style={{width:"90%", margin:"1rem"}}>
+                <img style={{padding:"1vw", borderRadius:"5%"}} src="https://images.ctfassets.net/wdjnw2prxlw8/6HRSjw4NJnvoQEKuDF9BsM/25bd19e9dfbfa9be0137096f74c454fa/glass_buildings_view_from_street_to_sky.jpg" className="card-img-top" alt="project" />
                 <div className="card-body">
                   <h6 className="card-title">Project1</h6>
                   <p className="card-text">RealtyAi</p>
-                  <Link className="my-button" to="http://localhost:3001/" role="button" target='_blank'>Compare Now</Link>
+                  <Link className="btn btn-outline-info center" to="https://otp-login-b8ec1.firebaseapp.com/" role="button" target='_blank'>Compare Now</Link>
                 </div>
               </div>
             </div>
